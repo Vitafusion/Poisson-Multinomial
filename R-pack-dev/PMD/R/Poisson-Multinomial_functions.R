3333789e556e0a49b39dded4dae569c91927963c
Lonely_Rifle <349461694@qq.com>
2021-08-06 16:00:54 -0400

    zz
    change dpmd(x,pp,..) to dpmd(pp,x,...)

--- R-pack-dev/PMD/R/Poisson-Multinomial_functions.R
+++ R-pack-dev/PMD/R/Poisson-Multinomial_functions.R
@@ -5,7 +5,7 @@
 #' is capable for computation of the whole probability mass function as well as
 #' of one single probability mass point. 
 #' 
-#' @param pp         A matrix of probabilities. Each row of pp should add up 
+#' @param pmat       A matrix of probabilities. Each row of pmat should add up 
 #'                   to 1.
 #' @param method     Character string stands for the method selected by user to 
 #'                   compute the probability mass. The method can only be one of 
@@ -14,10 +14,10 @@
 #'                   \code{"NA"},
 #'                   \code{"SIM"},
 #'                   \code{"SIM-ALL"}.
-#' @param vec        Result vector(probability mass point) specified by user when 
-#'                   the selected method is "SIM" or "NA". Eg. pp is 4 by 3 matrix 
-#'                   then a user might be interested in the probability of getting 
-#'                   result: vec=c(0,0,1,2).
+#' @param x          Result vector(probability mass point) specified by user when 
+#'                   the selected method is "SIM" or "NA". The vector 
+#'                   \code{x = (x_{1}, x_{2}, \ldots)} is used for computing 
+#'                   \eqn{P(X_{1}=x_{1}, X_{2}=x_{2}, \ldots)}.
 #' @param B          Simulation repeating time. Will be ignored if users do not
 #'                   choose \code{"SIM-ALL"} or \code{"SIM"}
 #'                   as method.
@@ -35,52 +35,52 @@
 #' accurate enough. Increasing \code{B} to larger than 10^8 will heavily aggravate 
 #' computation burden of a CPU or GPU. 
 #' 
-#' Given \code{pp} with dimension \eqn{n \times m}, the number of total probability 
-#' mass points is \eqn{(n+1)^{m-1}}. Thus when the dimension of \code{pp} 
+#' Given \code{pmat} with dimension \eqn{n \times m}, the number of total probability 
+#' mass points is \eqn{(n+1)^{m-1}}. Thus when the dimension of \code{pmat} 
 #' increases and the users selected method is one of \code{"DFT-CF"} and 
 #' \code{"SIM-ALL"}, the computation burden of \code{dpmd} might challenge the 
 #' capability of a computer because both of the methods calculate all probability 
 #' mass points of Poisson-Multinomial distributions.
 #' 
-#' \code{"NA"} specifies an approximation method using Normal 
-#' approximation to compute the probability mass point of the \code{vec} vector
-#' input by user.
 #'  
-#' \code{"SIM"} is as same as \code{"SIM-ALL"} except that it 
-#' only computes a single probability mass point specified by \code{vec}.
+#' \code{"SIM"} is as same as \code{"SIM-ALL"} except that it only computes a 
+#' single probability mass point specified by \code{vec}.
 #' 
+#' \code{"NA"} specifies an approximation method using Normal approximation to 
+#' compute the probability mass point of the \code{vec} vector
+#' input by user.
 #' 
 #' @return           
 #' For a single probability mass point, \code{dpmd} returns a probability value. 
 #' 
-#' For all probability mass points of a given \code{pp}, it returns a 
-#' multi-dimensional array. For instance, for the \code{pp} matrix in the 
+#' For all probability mass points of a given \code{pmat}, it returns a 
+#' multi-dimensional array. For instance, for the \code{pmat} matrix in the 
 #' following example, the value of the array element \eqn{a_{1,2,1}} = 0.90 means 
 #' the value of probability mass point (0,1,0,2) is 0.90. 
 #'                    
 #' @examples
-#' pp=matrix(c(.1, .1, .1, .7, .1, .3, .3, .3, .5, .2, .1, .2), nrow=3, byrow=TRUE)
+#' pp=matrix(c(.1, .1, .1, .7, .1, .3, .3, .3, .5, .2, .1, .2), nrow = 3, byrow = TRUE)
 #' 
 #' 
-#' dpmd(pp)
-#' dpmd(pp,"SIM-ALL",B=10^3)
-#' dpmd(pp,"NA", vec = c(0,0,1,2))
-#' dpmd(pp,"SIM", vec = c(0,0,1,2), B=10^3)
+#' dpmd(pmat = pp)
+#' dpmd(pmat = pp, method = "SIM-ALL", B = 10^3)
+#' dpmd(pmat = pp, x = c(0,0,1,2), method = "NA" )
+#' dpmd(pmat = pp, x = c(0,0,1,2), method = "SIM", B = 10^3)
 #' 
 #' @export
-dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
+dpmd <-function(pmat, x = c(0,0,0,0), method="DFT-CF", B=10^3)
 {
   
-  if(any(pp<0)|any(pp>1))
+  if(any(pmat<0)|any(pmat>1))
   {
-    stop("invalid values in pp.")
+    stop("invalid values in pmat.")
   }
   
   
   switch(method,
          "DFT-CF"={
-           mm=ncol(pp) # ncol of pp
-           nn=nrow(pp) # nrow of pp
+           mm=ncol(pmat) # ncol of pmat
+           nn=nrow(pmat) # nrow of pmat
            
            nn.vec=rep(nn+1, mm-1)
            l.vec=rep(0, mm-1)
@@ -93,7 +93,7 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
            
            #browser()
            
-           res0=pmn_mdfft_arma(nnt, pp, nn.vec, l.vec, cn.vec)
+           res0=pmn_mdfft_arma(nnt, pmat, nn.vec, l.vec, cn.vec)
            
            #example an_array[k + 27 * (j + 12 * i)]
            #print(round(res0, 9))
@@ -125,8 +125,8 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
            
          },
          "SIM-ALL"={
-             mm=ncol(pp) # ncol of pp
-             nn=nrow(pp) # nrow of pp
+             mm=ncol(pmat) # ncol of pmat
+             nn=nrow(pmat) # nrow of pmat
              nn.vec=rep(nn+1, mm-1)
              l.vec=rep(0, mm-1)
              cn.vec=cumprod(nn.vec)
@@ -135,7 +135,7 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
              cn.vec=as.integer(cn.vec) #((n+1)^(m-2),...,(n+1)^2,(n+1),1)
              nnt=prod(nn.vec) # (n+1)^(m-1) probability mass points
              
-             res0 = pmd_simulation_allpoints(pp, nnt, l.vec, cn.vec, B)
+             res0 = pmd_simulation_allpoints(pmat, nnt, l.vec, cn.vec, B)
              
              res=array(0, nn.vec)
              
@@ -163,14 +163,14 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
              res=round(res, 10)
          },
          "NA"=   {
-           mm=ncol(pp) # m categories
-           nn=nrow(pp) # n people
-           if(sum(vec)>nn|any(vec<0)|length(vec)!=mm)
+           mm=ncol(pmat) # m categories
+           nn=nrow(pmat) # n people
+           if(sum(x)>nn|any(x<0)|length(x)!=mm)
            {
              stop("invalid result vector")
            }
            mm = mm - 1
-           x_vec = vec[1:(length(vec)-1)]
+           x_vec = x[1:(length(x)-1)]
            lb = as.numeric(x_vec - 0.5)
            ub = as.numeric(x_vec+0.5)
            res = 0
@@ -178,23 +178,23 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
            # asymptotic sigma
            n = nn
            m = mm
-           if(n==1) pp = t(as.matrix(pp[,1:m])) else pp = as.matrix(pp[,1:m])
+           if(n==1) pmat = t(as.matrix(pmat[,1:m])) else pmat = as.matrix(pmat[,1:m])
            sig = matrix(0,m,m)
            for (i in 1:n) {
-             sig = sig + diag(pp[i,],nrow = m) - pp[i,]%*%t(pp[i,])
+             sig = sig + diag(pmat[i,],nrow = m) - pmat[i,]%*%t(pmat[i,])
            }
            
            # asymptotic mu
-           mu = matrix(0, nrow = 1, ncol = ncol(pp))
+           mu = matrix(0, nrow = 1, ncol = ncol(pmat))
            for (i in 1:n) {
-             mu = mu + pp[i,]
+             mu = mu + pmat[i,]
            }
            mu = as.vector(mu)
            res = mvtnorm::pmvnorm(lower=lb,upper = ub, mean = mu, sigma = sig)
            res = res[[1]]
          },
          "SIM" = {
-             res = pmd.by.demands(vec,pp,B)
+             res = pmd.by.demands(x,pmat,B)
          }
          
   )
@@ -211,14 +211,14 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
 #' Poisson-Multinomial distributions that specified by input probability matrix 
 #' via given method.
 #'  
-#' @param pp         A matrix of probabilities. Each row of pp should add up 
+#' @param pmat       A matrix of probabilities. Each row of pmat should add up 
 #'                   to 1.
 #' @param method     Character string stands for the method selected by user to 
 #'                   compute the probability mass. The method can only be one of 
-#'                   the following three,
-#'                   \code{"DFT-CF"}
-#'                   \code{"SIM-ALL"}
-#'                   \code{"NA"}
+#'                   the following three: 
+#'                   \code{"DFT-CF"},
+#'                   \code{"NA"},
+#'                   \code{"SIM-ALL"}. 
 #' @param B          Simulation repeating time. Will be ignored if users do not
 #'                   choose \code{"SIM-ALL"} as method.
 #' @param x          Vector \eqn{x = (x_{1},x_{2},\ldots)} for computing 
@@ -238,17 +238,16 @@ dpmd <-function(pp,method="DFT-CF",vec=c(0,0,0,0,0),B=100)
 #' @examples
 #' pp=matrix(c(.1, .1, .1, .7, .1, .3, .3, .3, .5, .2, .1, .2), nrow=3, byrow=TRUE)
 #' 
-#' 
-#' ppmd(pp, x = c(3,2,1,3))
-#' ppmd(pp, x = c(3,2,1,3), method = "SIM-ALL", B = 10^3)
-#' ppmd(pp, x = c(3,2,1,3), method = "NA")
+#' ppmd(pmat = pp, x = c(3,2,1,3))
+#' ppmd(pmat = pp, x = c(3,2,1,3), method = "NA")
+#' ppmd(pmat = pp, x = c(3,2,1,3), method = "SIM-ALL", B = 10^3)
 #' @export
-ppmd = function(pp,x,method="DFT-CF",B=1000){
-  if(any(pp<0)|any(pp>1)){
-    stop("invalid values in pp.")
+ppmd = function(pmat,x,method="DFT-CF",B=1000){
+  if(any(pmat<0)|any(pmat>1)){
+    stop("invalid values in pmat.")
   }
-  nn = nrow(pp)
-  mm = ncol(pp)
+  nn = nrow(pmat)
+  mm = ncol(pmat)
   if(any(x<0)){
     stop("invalid values in x.")
   }
@@ -292,7 +291,7 @@ ppmd = function(pp,x,method="DFT-CF",B=1000){
   points = idx[index,]
   switch(method,
          "DFT-CF" = {
-           res = dpmd(pp)
+           res = dpmd(pmat)
            temp.index = idx0[index,]
            prob  = 0
            res.expr="prob = prob + res[temp[1]"
@@ -316,14 +315,14 @@ ppmd = function(pp,x,method="DFT-CF",B=1000){
              points.pos = points[which(points[,mm]>=0),]
              prob = 0
              for(i in 1:nrow(points.pos)){
-               prob = prob + pmd.by.demands(as.numeric(points.pos[i,]),pp,T)
+               prob = prob + pmd.by.demands(as.numeric(points.pos[i,]),pmat,T)
            }
          },
          "NA" = {
            prob = 0
            points.pos = points[which(points[,mm]>=0),]
            for(i in 1:nrow(points.pos)){
-             prob = prob + dpmd(pp,method="NA",vec = points.pos[i,])
+             prob = prob + dpmd(pmat, x = points.pos[i,], method="NA")
            }
          })
   return(prob)
@@ -332,7 +331,7 @@ ppmd = function(pp,x,method="DFT-CF",B=1000){
 #' @title Poisson-Multinomial Distribution Random Number Generator
 #' @description Generating random samples of given a Poisson-Multinomial distribution.
 #'  
-#' @param pp         A matrix of probabilities. Each row of pp should add up 
+#' @param pmat       A matrix of probabilities. Each row of pmat should add up 
 #'                   to 1.
 #' @param n          Number of samples to be generated.
 #' 
@@ -342,19 +341,18 @@ ppmd = function(pp,x,method="DFT-CF",B=1000){
 #' @examples 
 #' pp=matrix(c(.1, .1, .1, .7, .1, .3, .3, .3, .5, .2, .1, .2), nrow=3, byrow=TRUE)
 #' 
-#' n=5
 #' 
-#' rpmd(pp,5)
+#' rpmd(n=5,pmat = pp)
 #' 
 #' @export
-rpmd = function(pp,n){
-  if(any(pp<0)|any(pp>1)){
-    stop("invalid values in pp.")
+rpmd = function(n,pmat){
+  if(any(pmat<0)|any(pmat>1)){
+    stop("invalid values in pmat.")
   }
-  mm = ncol(pp)
+  mm = ncol(pmat)
   rnd = matrix(NA,nrow = n,ncol = mm)
   for(i in 1:n){
-    rnd[i,] = t(rpmd_arma(pp))
+    rnd[i,] = t(rpmd_arma(pmat))
   }
   return(rnd)
 }
